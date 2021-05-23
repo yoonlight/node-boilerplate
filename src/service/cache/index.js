05472907c21471da07fbfcbcaef9f42b8ef0278e
kjw <yoonlight@gmail.com>
2021-05-24 01:46:25 +0900

    package update

---
+++ src/service/cache/index.js
@@ -0,0 +1,26 @@
+const cache = require('memory-cache')
+
+let memCache = new cache.Cache()
+
+let cacheMiddleware = (duration) => {
+  return (req, res, next) => {
+    let key = '__express__' + req.originalUrl || req.url
+    let cacheContent = memCache.get(key)
+    if (cacheContent) {
+      console.log('response cached data')
+      res.json(cacheContent)
+      return
+    } else {
+      res.sendResponse = res.json
+      res.json = (body) => {
+        memCache.put(key, body, duration * 1000)
+        res.sendResponse(body)
+      }
+      next()
+    }
+  }
+}
+
+module.exports = {
+  cacheMiddleware,
+}
